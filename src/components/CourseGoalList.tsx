feaa4362044253a4005cebf9614ab98f6fbff21e
Sara Costa Cabral Mululo <saramululo@gmail.com>
2025-07-10 14:42:30 -0500

    added discriminated unions to CourseGoalList

--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -1,13 +1,24 @@
 import CourseGoal from "./CourseGoal"
 import {type CourseGoal as CourseGoalType} from '../App';
+import InfoBox from "./InfoBox";
+import { ReactNode } from "react";
 
 type CourseGoalListProps = {
   goals: CourseGoalType[];
   onDeleteGoal:(id:number)=>void;
 };
 const CourseGoalList = ({goals, onDeleteGoal}: CourseGoalListProps) => {
+  if(goals.length===0) {
+    return <InfoBox mode='hint'>You have no course goals yet. Starting adding some!</InfoBox>
+  }
+  let warningBox:ReactNode;
+  if(goals.length>=4) {
+    warningBox=<InfoBox mode="warning" severity="medium">You're collecting a lot of goals. Don't put too much on your plate.</InfoBox>
+  }
   return (
-     <ul>
+    <>
+    {warningBox}
+    <ul>
         {goals.map((goal) => (
           <li key={goal.id}>
             <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
@@ -16,6 +27,8 @@ const CourseGoalList = ({goals, onDeleteGoal}: CourseGoalListProps) => {
           </li>
         ))}
       </ul>
+    </>
+     
   )
 }
 
